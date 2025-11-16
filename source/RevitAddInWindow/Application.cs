f96e3ad6910bf2c8fd9c0b8d509c8ef2e4d6dc1c
noy4 <naoyan418@gmail.com>
2025-11-16 23:09:01 +0800

    add project

---
+++ source/RevitAddInWindow/Application.cs
@@ -0,0 +1,26 @@
+﻿using Nice3point.Revit.Toolkit.External;
+using RevitAddInWindow.Commands;
+
+namespace RevitAddInWindow
+{
+    /// <summary>
+    ///     Application entry point
+    /// </summary>
+    [UsedImplicitly]
+    public class Application : ExternalApplication
+    {
+        public override void OnStartup()
+        {
+            CreateRibbon();
+        }
+
+        private void CreateRibbon()
+        {
+            var panel = Application.CreatePanel("Commands", "RevitAddInWindow");
+
+            panel.AddPushButton<StartupCommand>("Execute")
+                .SetImage("/RevitAddInWindow;component/Resources/Icons/RibbonIcon16.png")
+                .SetLargeImage("/RevitAddInWindow;component/Resources/Icons/RibbonIcon32.png");
+        }
+    }
+}
---
+++ source/RevitAddInWindow/Commands/StartupCommand.cs
@@ -0,0 +1,19 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI;
+using Nice3point.Revit.Toolkit.External;
+
+namespace RevitAddInWindow.Commands
+{
+    /// <summary>
+    ///     External command entry point
+    /// </summary>
+    [UsedImplicitly]
+    [Transaction(TransactionMode.Manual)]
+    public class StartupCommand : ExternalCommand
+    {
+        public override void Execute()
+        {
+            TaskDialog.Show(Document.Title, "RevitAddInWindow");
+        }
+    }
+}
